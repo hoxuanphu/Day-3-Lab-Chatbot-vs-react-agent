447b4e7177319ca609d9674d4a149b170b751271
hoxuanphu <hoxuanphu.stmn@gmail.com>
2026-04-06 10:24:49 +0700

    Update agent.py

--- src/agent/agent.py
+++ src/agent/agent.py
@@ -1,16 +1,17 @@
 import os
 import re
 from typing import List, Dict, Any, Optional
-from src.core.llm_provider import LLMProvider
+#from src.core.llm_provider import LLMProvider
 from src.telemetry.logger import logger
-
+from src.core.openai_provider import OpenAIProvider
+API_keys="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"
 class ReActAgent:
     """
     SKELETON: A ReAct-style Agent that follows the Thought-Action-Observation loop.
     Students should implement the core loop logic and tool execution.
     """
     
-    def __init__(self, llm: LLMProvider, tools: List[Dict[str, Any]], max_steps: int = 5):
+    def __init__(self, llm: OpenAIProvider, tools: List[Dict[str, Any]], max_steps: int = 5):
         self.llm = llm
         self.tools = tools
         self.max_steps = max_steps
